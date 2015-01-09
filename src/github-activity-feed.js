bcf7ae4c2aa6060738692169662174f07f91c262
Andrew Hood <andrewhood125@gmail.com>
2015-01-08 21:55:27 -0600

    Initial work on tests

--- src/github-activity-feed.js
+++ src/github-activity-feed.js
@@ -1,37 +1,38 @@
+var $ = require('jquery');
 // Static settings
-jQuery.timeago.settings.allowFuture = true;
 var GITHUB_API_BASE_URL = "https://api.github.com/";
 var GITHUB_BASE_URL = "https://github.com/";
 
-function author(event) {
+
+var author = function(event) {
   return event.actor.login;
 }
 
-function author_link(event) {
+var author_link = function(event) {
   return link(github_url(author(event)), author(event));
 }
 
-function ref(payload) {
+var ref = function(payload) {
   return payload.ref.replace("refs/heads/", '');
 }
 
-function ref_link(event) {
+var ref_link = function(event) {
   return link(github_url(repository(event.repo) + "/tree/" + ref(event.payload)), ref(event.payload));
 }
 
-function build_icon(icon_type) {
+var build_icon = function(icon_type) {
   return "<span class='" + icon_type + "'></span>";
 }
 
-function convert_api_url(url) {
+var convert_api_url = function(url) {
   return github_url(remove_api_url(url));
 }
 
-function forkee_link(forkee) {
+var forkee_link = function(forkee) {
   return link(github_url(forkee.full_name), forkee.full_name);
 }
 
-function gh_event(icon, text, timeago, at) {
+var gh_event = function(icon, text, timeago, at) {
   return {
     icon: build_icon(icon),
     text: text,
@@ -40,82 +41,82 @@ function gh_event(icon, text, timeago, at) {
   };
 }
 
-function gh_parse_CreateEvent(event) {
+var gh_parse_CreateEvent = function(event) {
   if (event.payload.ref_type == "repository")
     return gh_parse_CreateEvent_repository(event);
   return gh_parse_CreateEvent_tag(event);
 }
 
-function gh_parse_CreateEvent_repository(event) {
+var gh_parse_CreateEvent_repository = function(event) {
   return gh_event('octicon octicon-repo',
     author_link(event) + " created repository " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_CreateEvent_tag(event) {
+var gh_parse_CreateEvent_tag = function(event) {
   return gh_event('octicon octicon-tag',
     author_link(event) + " created tag " + ref_link(event) + " at " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_DeleteEvent(event) {
+var gh_parse_DeleteEvent = function(event) {
   return gh_event('octicon octicon-git-branch-delete',
     author_link(event) + " deleted " + ref_type(event.payload) + " " + ref(event.payload) + " at " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_ForkEvent(event) {
+var gh_parse_ForkEvent = function(event) {
   return gh_event('octicon octicon-git-branch',
     author_link(event) + " forked " + repository_link(event) + " to " + forkee_link(event.payload.forkee),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_IssueCommentEvent(event) {
+var gh_parse_IssueCommentEvent = function(event) {
   return gh_event('mega-octicon octicon-comment-discussion',
     author_link(event) + " commened on pull request " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_IssuesEvent(event) {
+var gh_parse_IssuesEvent = function(event) {
   if (event.payload.action == "opened")
     return gh_parse_IssuesEvent_opened(event);
   return gh_parse_IssuesEvent_closed(event);
 }
 
-function gh_parse_IssuesEvent_opened(event) {
+var gh_parse_IssuesEvent_opened = function(event) {
   return gh_event('mega-octicon octicon-issue-opened',
     author_link(event) + " opened issue " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_IssuesEvent_closed(event) {
+var gh_parse_IssuesEvent_closed = function(event) {
   return gh_event('mega-octicon octicon-issue-closed',
     author_link(event) + " closed issue " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_PushEvent(event) {
+var gh_parse_PushEvent = function(event) {
   return gh_event('mega-octicon octicon-git-commit',
     author_link(event) + " pushed to " + ref_link(event) + " at " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_WatchEvent(event) {
+var gh_parse_WatchEvent = function(event) {
   return gh_event('octicon octicon-star',
     author_link(event) + " starred " + repository_link(event),
     time_since(event),
     event.created_at);
 }
 
-function gh_parse_UnknownEvent(event) {
+var gh_parse_UnknownEvent = function(event) {
   return gh_event('', 'Unknown Event', time_since(event), event.created_at);
 }
 
@@ -148,11 +149,11 @@ function GithubActivityFeed(username) {
   self.refresh();
 }
 
-function github_url(resource) {
+var github_url = function(resource) {
   return GITHUB_BASE_URL + resource;
 }
 
-function human_readable(data) {
+var human_readable = function(data) {
   var events = [];
 
   for (var i = 0; i < data.length; i++) {
@@ -166,33 +167,52 @@ function human_readable(data) {
   return events;
 }
 
-function link(url, name) {
+var link = function(url, name) {
   return "<a href='" + url + "'>" + name + "</a>";
 }
 
-function ref_type(payload) {
+var ref_type = function(payload) {
   return payload.ref_type;
 }
 
-function remove_api_url(url) {
+var remove_api_url = function remove_api_url(url) {
   return url.replace(GITHUB_API_BASE_URL, '').replace("repos/", '');;
 }
 
-function repository(repo) {
+var repository = function(repo) {
   return repo.name;
 }
 
-function repository_link(event) {
+var repository_link = function(event) {
   return link(convert_api_url(event.repo.url), repository(event.repo));
 }
 
-function time_since(event) {
+var time_since = function(event) {
   return "<span class='timeago'> " + $.timeago(event.created_at) + "</span>";
 }
 
-function truncate(string) {
+var truncate = function(string) {
   var max_length = 50;
   if (string.length < max_length)
     return string;
   return string.substring(0, 50) + "...";
 }
+
+module.exports = {
+  author: author,
+  author_link: author_link,
+  ref: ref,
+  ref_link: ref_link,
+  build_icon: build_icon,
+  convert_api_url: convert_api_url,
+  remove_api_url: remove_api_url,
+  forkee_link: forkee_link,
+  gh_event: gh_event,
+  github_url: github_url,
+  link: link,
+  ref_type: ref_type,
+  repository: repository,
+  repository_link: repository_link,
+  time_since: time_since,
+  truncate: truncate
+};
--- test/test.js
+++ test/test.js
@@ -1,9 +1,118 @@
 var assert = require("assert")
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3].indexOf(5));
-      assert.equal(-1, [1, 2, 3].indexOf(0));
+var gaf = require('../src/github-activity-feed');
+
+
+var event = {
+  actor: {
+    login: "andrewhood125"
+  },
+  repo: {
+    name: "andrewhood125/github-activity-feed",
+    url: "https://api.github.com/repos/andrewhood125/github-activity-feed"
+  },
+  payload: {
+    forkee: {
+      full_name: "andrewhood125/common-critic"
+    },
+    ref: "refs/heads/master",
+    ref_type: "repository"
+  }
+};
+
+describe('GitHub Activity Feed', function() {
+  describe('#author()', function() {
+    it('should return andrewhood125', function() {
+      assert.equal("andrewhood125", gaf.author(event));
     })
-  })
+  });
+
+  describe('#author_link()', function() {
+    it('should return link to username', function() {
+      assert.equal("<a href='https://github.com/andrewhood125'>andrewhood125</a>", gaf.author_link(event));
+    })
+  });
+
+  describe('#ref()', function() {
+    it('should return master', function() {
+      assert.equal("master", gaf.ref(event.payload));
+    })
+  });
+
+  describe('#ref_link()', function() {
+    it('should return link to ref', function() {
+      assert.equal("<a href='https://github.com/andrewhood125/github-activity-feed/tree/master'>master</a>", gaf.ref_link(event));
+    });
+  });
+
+  describe('#build_icon()', function() {
+    it('should return html for octicon', function() {
+      assert.equal("<span class='mega-octicon octicon-git-commit'></span>", gaf.build_icon('mega-octicon octicon-git-commit'));
+    });
+  });
+
+  describe('#convert_api_url()', function() {
+    it('should return a github url with no subdomain', function() {
+      assert.equal('https://github.com/', gaf.convert_api_url('https://api.github.com/'));
+    });
+  });
+
+  describe('#remove_api_url()', function() {
+    it('should strip the api subdomain from a github url', function() {
+      assert.equal('andrewhood125', gaf.remove_api_url('https://api.github.com/andrewhood125'));
+    });
+  });
+
+  describe('#forkee_link()', function() {
+    it('should return link to fork', function() {
+      assert.equal("<a href='https://github.com/andrewhood125/common-critic'>andrewhood125/common-critic</a>", gaf.forkee_link(event.payload.forkee));
+    });
+  });
+
+  describe('#gh_event()', function() {
+    it('should return a single event object', function() {
+      var e = gaf.gh_event('commit', 'star', '5 days', '12pm');
+      assert.equal("<span class='commit'></span>", e.icon);
+      assert.equal('star', e.text);
+      assert.equal('5 days', e.timeago);
+      assert.equal('12pm', e.at);
+    });
+  });
+
+  describe('#github_url()', function() {
+    it('should return a github url for a resource', function() {
+      assert.equal('https://github.com/andrewhood125', gaf.github_url('andrewhood125'));
+    });
+  });
+
+  describe('#link()', function() {
+    it('should return html link for url and name', function() {
+      assert.equal("<a href='andrew-hood.com'>Andrew Hood</a>", gaf.link('andrew-hood.com', 'Andrew Hood'));
+    });
+  });
+
+  describe('#ref_type()', function() {
+    it('should return ref_type for the event', function() {
+      assert.equal('repository', gaf.ref_type(event.payload));
+    });
+  });
+
+  describe('#repository()', function() {
+    it('should return the name of the repository', function() {
+      assert.equal('andrewhood125/github-activity-feed', gaf.repository(event.repo));
+    });
+  });
+
+  describe('#repository_link()', function() {
+    it('should return a link to the repository', function() {
+      assert.equal("<a href='https://github.com/andrewhood125/github-activity-feed'>andrewhood125/github-activity-feed</a>", gaf.repository_link(event));
+    });
+  });
+
+  describe('#truncate()', function() {
+    it('should truncate strings longer than 50 chars', function() {
+      assert.equal('Short text', gaf.truncate('Short text'));
+      assert.equal('12345678910111213141516171819202122232425262728293...', gaf.truncate('123456789101112131415161718192021222324252627282930'));
+    });
+  });
+
 })
