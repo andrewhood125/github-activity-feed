27e6e2a0d206c76f35120406e7a3af3b76189ae0
Andrew Hood <andrewhood125@gmail.com>
2015-01-27 22:28:37 -0600

    Missing semicolon

--- src/github-activity-feed.js
+++ src/github-activity-feed.js
@@ -25,45 +25,45 @@
 
     self.abbr_sha = function(sha) {
       return self.truncate(sha, 6, false);
-    }
+    };
 
     self.action = function(payload) {
       return payload.action;
-    }
+    };
 
     self.author = function(event) {
       return event.actor.login;
-    }
+    };
 
     self.author_link = function(event) {
       return self.link(self.github_url(self.author(event)), self.author(event));
-    }
+    };
 
     self.build_icon = function(icon_type) {
       return "<span class='" + icon_type + "'></span>";
-    }
+    };
 
     self.convert_api_url = function(url) {
       return self.github_url(self.remove_api_url(url));
-    }
+    };
 
     self.details_IssueCommentEvent = function(event) {
       var details  = [];
       details.push(event.payload.comment.body);
       return details;
-    }
+    };
 
     self.details_IssuesEvent = function(event) {
       var details = [];
       details.push(event.payload.issue.title);
       return details;
-    }
+    };
 
     self.details_PullRequestEvent = function(event) {
       var details = [];
       details.push(event.payload.pull_request.title);
       return details;
-    }
+    };
 
     self.details_PushEvent = function(event) {
       var details = [];
@@ -73,11 +73,11 @@
           self.link(self.convert_api_url(commit.url), self.abbr_sha(commit.sha)) + " " + self.truncate(commit.message));
       }
       return details;
-    }
+    };
 
     self.forkee_link = function(forkee) {
       return self.link(self.github_url(forkee.full_name), forkee.full_name);
-    }
+    };
 
     self.gh_event = function(icon, text, event, details) {
       return {
@@ -86,71 +86,71 @@
         timeago: self.time_since(event),
         details: details
       };
-    }
+    };
 
     self.gh_parse_CreateEvent = function(event) {
       if (event.payload.ref_type == "repository")
         return self.gh_parse_CreateEvent_repository(event);
       return self.gh_parse_CreateEvent_tag(event);
-    }
+    };
 
     self.gh_parse_CreateEvent_repository = function(event) {
       return self.gh_event('octicon octicon-repo',
         self.author_link(event) + " created repository " + self.repository_link(event),
         event);
-    }
+    };
 
     self.gh_parse_CreateEvent_tag = function(event) {
       return self.gh_event('octicon octicon-tag',
         self.author_link(event) + " created tag " + self.ref_link(event) + " at " + self.repository_link(event),
         event);
-    }
+    };
 
     self.gh_parse_DeleteEvent = function(event) {
       return self.gh_event('octicon octicon-git-branch-delete',
         self.author_link(event) + " deleted " + self.ref_type(event.payload) + " " + self.ref(event.payload) + " at " + self.repository_link(event),
         event);
-    }
+    };
 
     self.gh_parse_ForkEvent = function(event) {
       return self.gh_event('octicon octicon-git-branch',
         self.author_link(event) + " forked " + self.repository_link(event) + " to " + self.forkee_link(event.payload.forkee),
         event);
-    }
+    };
 
     self.gh_parse_IssueCommentEvent = function(event) {
       return self.gh_event('mega-octicon octicon-comment-discussion',
         self.author_link(event) + " commened on pull request " + self.repository_link(event),
         event,
         self.details_IssueCommentEvent(event));
-    }
+    };
 
     self.gh_parse_IssuesEvent = function(event) {
       if (event.payload.action == "opened")
         return self.gh_parse_IssuesEvent_opened(event);
       return self.gh_parse_IssuesEvent_closed(event);
-    }
+    };
 
     self.gh_parse_IssuesEvent_opened = function(event) {
       return self.gh_event('mega-octicon octicon-issue-opened',
         self.author_link(event) + " opened issue " + self.repository_link(event),
         event,
         self.details_IssuesEvent(event));
-    }
+    };
 
     self.gh_parse_IssuesEvent_closed = function(event) {
       return self.gh_event('mega-octicon octicon-issue-closed',
         self.author_link(event) + " closed issue " + self.repository_link(event),
         event,
         self.details_IssuesEvent(event));
-    }
+    };
 
     self.gh_parse_PushEvent = function(event) {
       return self.gh_event('mega-octicon octicon-git-commit',
         self.author_link(event) + " pushed to " + self.ref_link(event) + " at " + self.repository_link(event),
         event,
         self.details_PushEvent(event));
-    }
+    };
 
     self.gh_parse_PullRequestEvent = function(event) {
       if (self.pull_request_closed(event.payload))
@@ -159,7 +159,7 @@
         self.author_link(event) + " " + self.action(event.payload) + " pull request " + self.pull_request_link(event.payload.pull_request),
         event,
         self.details_PullRequestEvent(event));
-    }
+    };
 
     self.gh_parse_PullRequestEvent_closed = function(event) {
       var action = " closed ";
@@ -169,21 +169,21 @@
         self.author_link(event) + action + "pull request " + self.pull_request_link(event.payload.pull_request),
         event,
         self.details_PullRequestEvent(event));
-    }
+    };
 
     self.gh_parse_WatchEvent = function(event) {
       return self.gh_event('octicon octicon-star',
         self.author_link(event) + " starred " + self.repository_link(event),
         event);
-    }
+    };
 
     self.gh_parse_UnknownEvent = function(event) {
       return self.gh_event('mega-octicon octicon-bug', event.type + " not yet implemented. Submit an <a href='https://github.com/andrewhood125/github-activity-feed/issues/new'>issue</a>!", event);
-    }
+    };
 
     self.github_url = function(resource) {
       return GITHUB_BASE_URL + resource;
-    }
+    };
 
     self.human_readable = function(data) {
       var events = [];
@@ -195,51 +195,51 @@
           events[i] = self.gh_parse_UnknownEvent(data[i]);
       }
       return events;
-    }
+    };
 
     self.link = function(url, name) {
       return "<a href='" + url + "'>" + name + "</a>";
-    }
+    };
 
     self.pull_request_closed = function(payload) {
       if (payload.action === "closed")
         return true;
       return false;
-    }
+    };
 
     self.pull_request_link = function(pull_request) {
       return self.link(pull_request._links.html.href, pull_request.title);
-    }
+    };
 
     self.rate_limit_exceeded = function(meta) {
       if (meta["X-RateLimit-Remaining"] == "0")
         return true;
       return false;
-    }
+    };
 
     self.ref = function(payload) {
       return payload.ref.replace("refs/heads/", '');
-    }
+    };
 
     self.ref_link = function(event) {
       return self.link(self.github_url(self.repository(event.repo) + "/tree/" + self.ref(event.payload)), self.ref(event.payload));
-    }
+    };
 
     self.ref_type = function(payload) {
       return payload.ref_type;
-    }
+    };
 
     self.remove_api_url = function remove_api_url(url) {
       return url.replace(GITHUB_API_BASE_URL, '').replace("repos/", '');;
-    }
+    };
 
     self.repository = function(repo) {
       return repo.name;
-    }
+    };
 
     self.repository_link = function(event) {
       return self.link(self.convert_api_url(event.repo.url), self.repository(event.repo));
-    }
+    };
 
     self.resolve_events = function(events, status) {
       if (self.rate_limit_exceeded(events.meta)) {
@@ -256,7 +256,7 @@
         meta: events.meta,
         data: self.human_readable(events.data)
       });
-    }
+    };
 
 
     self.resolve_user = function(user, status) {
@@ -271,16 +271,16 @@
           }
         });
       return self.user.resolve(user);
-    }
+    };
 
     self.time_since = function(event) {
       return "<span class='timeago'> " + moment(event.created_at).fromNow() + "</span>";
-    }
+    };
 
     self.time_until_api_refresh = function(meta) {
       var epoch = 1000 * parseInt(meta['X-RateLimit-Reset']);
       return moment(epoch).fromNow();
-    }
+    };
 
     self.truncate = function(string, length, dots) {
       var length = typeof length !== 'undefined' ? length : 50;
@@ -292,6 +292,6 @@
         short_string += "...";
       return short_string;
     }
-  }
+  };
 
 }(this));
